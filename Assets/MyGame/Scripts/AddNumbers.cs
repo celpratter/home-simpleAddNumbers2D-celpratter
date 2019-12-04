b5584046741b42601eaadf60858274b46c542925
celpratter <celina.pratter@htl-salzburg.ac.at>
2019-12-04 12:31:40 +0100

    update script, buttons getting inactive

--- Assets/MyGame/Scripts/AddNumbers.cs
+++ Assets/MyGame/Scripts/AddNumbers.cs
@@ -8,12 +8,18 @@ public class AddNumbers : MonoBehaviour
     public InputField ipVarA;
     public InputField ipVarB;
     public Text result;
+    public Button btn_add;
+    public Button btn_reset;
 
     public void SetResult()
     {
         float a = float.Parse(ipVarA.text);
         float b = float.Parse(ipVarB.text);
         result.text = NumbersAdd(a, b).ToString();
+        ipVarA.interactable = false;
+        ipVarB.interactable = false;
+        btn_add.interactable = false;
+        btn_reset.interactable = true;
     }
 
     private float NumbersAdd(float variableA, float variableB)
